4988b817dde9c93f4c9aa114146b9c60b21f663b
Maaz Nasir <maazn@udel.edu>
2018-04-22 19:21:47 -0400

    new classes in excel project

---
+++ CSBA_Excel/src/main/java/pkgExcel/Player.java
@@ -0,0 +1,34 @@
+package pkgExcel;
+
+public class Player {
+	
+	int player_id;
+	String player_guid;
+	String first_name;
+	String last_name;
+	
+	public int getPlayer_id() {
+		return player_id;
+	}
+	public void setPlayer_id(int player_id) {
+		this.player_id = player_id;
+	}
+	public String getPlayer_guid() {
+		return player_guid;
+	}
+	public void setPlayer_guid(String player_guid) {
+		this.player_guid = player_guid;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+}
